ac64cbf3a170fa7bb145c9ea75321b7774146f81
Drew Cuthbertson <syeoryn@gmail.com>
2015-05-18 19:55:45 -0700

    Implement solution

--- src/prefixTree.js
+++ src/prefixTree.js
@@ -3,13 +3,60 @@ var PrefixTree = function(){
 }
 
 PrefixTree.prototype.addWord = function(word){
-  /* Implement me! */
+  if(word){
+    var firstLetter = word[0];
+    var remainder = word.slice(1);
+    if(!this.children[firstLetter]){
+      this.children[firstLetter] = new PrefixTree();
+    }
+    this.children[firstLetter].addWord(remainder);
+  } else {
+    this.children['end'] = '*'
+  }
+
+  return word;
 }
 
 PrefixTree.prototype.getWords = function(prefix){
-  /* Implement me! */
+  var words = [];
+  var suffixes;
+  if(prefix){
+    var letter = prefix[0];
+    if(this.children[letter]){
+      suffixes = this.children[letter].getWords(prefix.slice(1));
+      suffixes.forEach(function(suffix){
+        words.push(letter + suffix);
+      });
+    }
+  } else{
+    for(var letter in this.children){
+      if(letter === 'end'){
+        words.push('*');
+      } else {
+        suffixes = this.children[letter].getWords();
+        suffixes.forEach(function(suffix){
+          words.push(letter + suffix);
+        });
+      }
+    }
+  }
+
+  words = words.map(function(word){
+    return word.replace('*','');
+  });
+  return words;
 }
 
 PrefixTree.prototype.removeWord = function(word){
-  /* I'm extra credit! */
+  if(word){
+    if(word[0] in this.children){
+      this.children[word[0]].removeWord(word.slice(1));
+    }
+  }
+
+  if(!word || Object.keys(this.children).length === 1){
+    delete this.children['end'];
+  }
+
+  return word;
 }
